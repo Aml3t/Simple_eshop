724ddd7a4f78ef71f13eba3af78e5c895850a8b6
Aml3t <fischerparsos@gmail.com>
2024-01-18 13:01:49 +0200

    Editing the project

--- Simple_eshop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Simple_eshop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
