e5f87360a800f193429ea4a9915864a4bf0ff6c0
Aml3t <fischerparsos@gmail.com>
2024-01-19 14:44:44 +0200

    Model creation

---
+++ Simple_eshop/Models/MockPieRepository.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Simple_Eshop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie{PieId = 1, Name = "Test Pie", CategoryId = 1 }
+            };
+            
+
+        public IEnumerable<Pie> PiesOfTheWeek => throw new NotImplementedException();
+
+        public Pie? GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
