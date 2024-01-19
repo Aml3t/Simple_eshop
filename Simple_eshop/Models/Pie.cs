8f6f90007c0afa2f01715fe10d1eb3574bf71421
Aml3t <fischerparsos@gmail.com>
2024-01-19 13:28:22 +0200

    Model creation

---
+++ Simple_eshop/Models/Pie.cs
@@ -0,0 +1,14 @@
+﻿namespace Simple_Eshop.Models
+{
+    public class Pie
+    {
+        public int PieId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public string? AllergyInformation { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
