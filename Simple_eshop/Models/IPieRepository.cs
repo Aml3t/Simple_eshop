0bb70227ae2344a7d141df4b80db25a3f621863b
Aml3t <fischerparsos@gmail.com>
2024-01-19 13:52:14 +0200

    Model creation

---
+++ Simple_eshop/Models/IPieRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Simple_Eshop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie? GetPieById(int pieId);
+    }
+}
