4ff3aa0212b1d3041ca0fc6ba5d34d1f2ea2c834
Aml3t <fischerparsos@gmail.com>
2024-01-22 12:40:21 +0200

    Adding controllers

--- Simple_eshop/Controllers/PieController.cs
+++ Simple_eshop/Controllers/PieController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Simple_Eshop.Models;
 
 namespace Simple_Eshop.Controllers
 {
     public class PieController : Controller
     {
-        public IActionResult Index()
+        private readonly IPieRepository _pieRepository;
+
+        public PieController(IPieRepository pieRepository)
         {
-            return View();
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Action()
+        {
+
         }
     }
 }
