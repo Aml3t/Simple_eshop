3e2a47814a19a8c252ab8927ee8dd54fe779e8cd
Aml3t <fischerparsos@gmail.com>
2024-01-26 11:41:47 +0200

    Adding EF Core

--- Simple_eshop/Models/BethanysPieShopDbContext.cs
+++ Simple_eshop/Models/BethanysPieShopDbContext.cs
@@ -7,7 +7,10 @@ namespace Simple_Eshop.Models
         public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> 
             options) : base(options)
         {
-                
         }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+
     }
 }
