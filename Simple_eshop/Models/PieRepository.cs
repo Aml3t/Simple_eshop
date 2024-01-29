1ad6c939e9fcc51c09e93b90cf76dd745854227a
Aml3t <fischerparsos@gmail.com>
2024-01-29 13:04:28 +0200

    Adding EF Core

--- Simple_eshop/Models/PieRepository.cs
+++ Simple_eshop/Models/PieRepository.cs
@@ -2,5 +2,11 @@
 {
     public class PieRepository : IPieRepository
     {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+
+        public PieRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+        }
     }
 }
