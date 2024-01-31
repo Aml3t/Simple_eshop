72ef1fc473cf96d66d9516b9bf67c708fa08c5c9
Aml3t <fischerparsos@gmail.com>
2024-01-31 11:15:52 +0200

    Adding Tag Helpers

---
+++ Simple_eshop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_Eshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
