63c5dff8b3b1264ac11ae279000592b721b985c4
Aml3t <fischerparsos@gmail.com>
2024-02-01 12:48:30 +0200

    Adding Cart

---
+++ Simple_eshop/Models/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Simple_Eshop.Models
+{
+    public class ShoppingCart : IShoppingCart
+    {
+        public List<ShoppingCartItem> ShoppingCartItems { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AddToCart(Pie pie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromCart(Pie pie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
