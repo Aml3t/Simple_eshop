0d8ce7289838a7058ee877a5674bb52228caba41
Aml3t <fischerparsos@gmail.com>
2024-02-01 11:22:01 +0200

    Adding Partial Views

---
+++ Simple_eshop/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Simple_Eshop.Models
+{
+    public class ShoppingCartItem
+    {
+        public int  ShoppingCartItemId { get; set; }
+        public Pie Pie { get; set; } = default!;
+        public int Amount { get; set; }
+        public string? ShoppingCartId { get; set; }
+
+    }
+}
+
