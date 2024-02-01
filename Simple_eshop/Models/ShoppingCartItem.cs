d9cc1ccf6069377cb5fc96b93b64884d5c4db415
Aml3t <fischerparsos@gmail.com>
2024-02-01 11:25:28 +0200

    Adding Cart

--- Simple_eshop/Models/ShoppingCartItem.cs
+++ Simple_eshop/Models/ShoppingCartItem.cs
@@ -2,7 +2,7 @@
 {
     public class ShoppingCartItem
     {
-        public int  ShoppingCartItemId { get; set; }
+        public int ShoppingCartItemId { get; set; }
         public Pie Pie { get; set; } = default!;
         public int Amount { get; set; }
         public string? ShoppingCartId { get; set; }
