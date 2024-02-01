562431727ffa1cc27c8edfbc7ccd7fb51656c73f
Aml3t <fischerparsos@gmail.com>
2024-02-01 11:29:52 +0200

    Adding Cart

--- Simple_eshop/Migrations/20240201092838_AddShoppingCartItem.Designer.cs
+++ Simple_eshop/Migrations/20240201092838_AddShoppingCartItem.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Simple_Eshop.Models;
 
@@ -10,9 +11,10 @@ using Simple_Eshop.Models;
 namespace Simple_Eshop.Migrations
 {
     [DbContext(typeof(BethanysPieShopDbContext))]
-    partial class BethanysPieShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240201092838_AddShoppingCartItem")]
+    partial class AddShoppingCartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
