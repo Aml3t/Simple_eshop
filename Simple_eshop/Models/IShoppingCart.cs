10beb11e8ae6c556d09b34cfc3495fc671b7cbb1
Aml3t <fischerparsos@gmail.com>
2024-02-01 12:32:07 +0200

    Adding Cart

--- Simple_eshop/Models/IShoppingCart.cs
+++ Simple_eshop/Models/IShoppingCart.cs
@@ -2,6 +2,12 @@
 {
     public interface IShoppingCart
     {
+        void AddToCart(Pie pie);
+        void RemoveFromCart(Pie pie);
+        List<ShoppingCartItem> GetShoppingCartItems();
+        void ClearCart();
+        decimal GetShoppingCartTotal();
+        List<ShoppingCartItem> ShoppingCartItems { get; set; }
 
     }
 }
