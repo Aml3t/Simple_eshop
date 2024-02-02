ad742d5d0cbf3e8bb61da4d7381b8747ac76a7eb
Aml3t <fischerparsos@gmail.com>
2024-02-02 12:55:41 +0200

    Adding Cart

---
+++ Simple_eshop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Simple_Eshop.Models;
+
+namespace Simple_Eshop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly IShoppingCart _shoppingCart;
+    }
+}
