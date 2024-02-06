de102d3a080f2dc7bf65a08d9b030c8131129d8f
Aml3t <fischerparsos@gmail.com>
2024-02-06 12:40:52 +0200

    Adding Cart

---
+++ Simple_eshop/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Simple_Eshop.Models;
+
+namespace Simple_Eshop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public IShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+        public ShoppingCartViewModel(IShoppingCart shoppingCart, decimal shoppingCartTotal)
+        {
+            ShoppingCart = shoppingCart;
+            ShoppingCartTotal = shoppingCartTotal;
+        }
+    }
+}
