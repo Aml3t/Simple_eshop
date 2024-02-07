7a9c8bcde71d23392f80eb8b3d97ac1b1c84e9bc
Aml3t <fischerparsos@gmail.com>
2024-02-07 12:49:42 +0200

    Adding View Components

--- Simple_eshop/Components/ShoppingCartSummary.cs
+++ Simple_eshop/Components/ShoppingCartSummary.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Simple_Eshop.Models;
 
 namespace Simple_Eshop.Components
 {
     public class ShoppingCartSummary: ViewComponent
     {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
     }
 }
