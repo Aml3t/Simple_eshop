a6e5fd7afa48a1caf2a50aae6b8208c59ae179b5
Aml3t <fischerparsos@gmail.com>
2024-02-08 15:10:53 +0200

    Adding Tag helpers

--- Simple_eshop/TagHelpers/EmailTagHelper.cs
+++ Simple_eshop/TagHelpers/EmailTagHelper.cs
@@ -2,14 +2,17 @@
 
 namespace Simple_Eshop.TagHelpers
 {
-    public class EmailTagHelper: TagHelper
+    public class EmailTagHelper : TagHelper
     {
         public string? Address { get; set; }
         public string? Content { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            
+            output.TagName = "a";
+
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(Content);
+
         }
     }
-}
