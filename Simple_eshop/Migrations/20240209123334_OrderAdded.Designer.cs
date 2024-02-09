1cad25df1ef30c081b42a6e35c84fd6e9559e229
Aml3t <fischerparsos@gmail.com>
2024-02-09 14:33:53 +0200

    Creating Order form

--- Simple_eshop/Migrations/20240209123334_OrderAdded.Designer.cs
+++ Simple_eshop/Migrations/20240209123334_OrderAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Simple_Eshop.Models;
 
@@ -11,9 +12,10 @@ using Simple_Eshop.Models;
 namespace Simple_Eshop.Migrations
 {
     [DbContext(typeof(BethanysPieShopDbContext))]
-    partial class BethanysPieShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240209123334_OrderAdded")]
+    partial class OrderAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
