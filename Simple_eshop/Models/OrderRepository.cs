c682de9263c1a1f1c54195115c3184e80c9f04f1
Aml3t <fischerparsos@gmail.com>
2024-02-09 14:15:49 +0200

    Creating Order form

--- Simple_eshop/Models/OrderRepository.cs
+++ Simple_eshop/Models/OrderRepository.cs
@@ -2,10 +2,36 @@
 {
     public class OrderRepository : IOrderRepository
     {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+        private readonly IShoppingCart _shoppingCart;
+        public OrderRepository(BethanysPieShopDbContext bethanysPieShopDbContext, IShoppingCart shoppingCart)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+            _shoppingCart = shoppingCart;
+        }
 
         public void CreateOrder(Order order)
         {
-            throw new NotImplementedException();
+            order.OrderPlaced = DateTime.Now;
+            List<ShoppingCartItem> shoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            order.OrderDetails = new List<OrderDetail>();
+
+            //adding orders with its details
+
+            foreach (ShoppingCartItem? shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetails = new OrderDetail
+                {
+                    Amount = shoppingCartItem.Amount,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    Price = shoppingCartItem.Pie.Price
+                };
+
+                order.OrderDetails.Add(orderDetails);
+            }
+
+
         }
     }
 }
