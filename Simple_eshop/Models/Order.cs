3244e087e14ed25306a19523cf67950d462dfed0
Aml3t <fischerparsos@gmail.com>
2024-02-09 13:27:34 +0200

    Creating Order form

--- Simple_eshop/Models/Order.cs
+++ Simple_eshop/Models/Order.cs
@@ -2,6 +2,12 @@
 {
     public class Order
     {
+        public int OrderId { get; set; }
+        public List<OrderDetail>? OrderDetails { get; set; }
 
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string? AddressLine2 { get; set; }
     }
 }
