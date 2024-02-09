c3cf457c6429b5268a9757826526ee926d8840f5
Aml3t <fischerparsos@gmail.com>
2024-02-09 13:29:18 +0200

    Creating Order form

--- Simple_eshop/Models/Order.cs
+++ Simple_eshop/Models/Order.cs
@@ -9,5 +9,11 @@
         public string LastName { get; set; } = string.Empty;
         public string AddressLine { get; set; } = string.Empty;
         public string? AddressLine2 { get; set; }
+        public string ZipCode { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string? State { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
     }
 }
