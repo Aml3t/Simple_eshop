a75a023991b0b80d6edaa548f01b6d5b0253af17
Aml3t <fischerparsos@gmail.com>
2024-02-09 13:42:16 +0200

    Creating Order form

---
+++ Simple_eshop/Models/OrderRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Simple_Eshop.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+
+        public void CreateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
