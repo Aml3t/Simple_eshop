b98e848f2dd91b13aa32a70b7fe0b0dcb5f14dcf
Aml3t <fischerparsos@gmail.com>
2024-02-12 12:35:30 +0200

    Creating Order form

---
+++ Simple_eshop/Migrations/20240212103203_OrderAdded2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simple_Eshop.Migrations
+{
+    public partial class OrderAdded2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AddressLine",
+                table: "Orders",
+                newName: "AddressLine1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AddressLine1",
+                table: "Orders",
+                newName: "AddressLine");
+        }
+    }
+}
--- Simple_eshop/Models/Order.cs
+++ Simple_eshop/Models/Order.cs
@@ -6,7 +6,7 @@
         public List<OrderDetail>? OrderDetails { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string AddressLine { get; set; } = string.Empty;
+        public string AddressLine1 { get; set; } = string.Empty;
         public string? AddressLine2 { get; set; }
         public string ZipCode { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
