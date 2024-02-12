0b956100636094e72d9aa2728c6f44b19b2b1ee0
Aml3t <fischerparsos@gmail.com>
2024-02-12 14:55:51 +0200

    Accessing posted data using model binding

--- Simple_eshop/Models/Order.cs
+++ Simple_eshop/Models/Order.cs
@@ -1,9 +1,18 @@
-﻿namespace Simple_Eshop.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Simple_Eshop.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderId { get; set; }
         public List<OrderDetail>? OrderDetails { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string AddressLine1 { get; set; } = string.Empty;
