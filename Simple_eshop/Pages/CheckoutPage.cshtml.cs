91c64560029407a89f322dd3a9d1b691a82f2920
Aml3t <fischerparsos@gmail.com>
2024-02-13 14:34:15 +0200

    Recreating the Form using Razor Pages

--- Simple_eshop/Pages/CheckoutPage.cshtml.cs
+++ Simple_eshop/Pages/CheckoutPage.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Simple_Eshop.Models;
 
 namespace Simple_Eshop.Pages
 {
     public class CheckoutPageModel : PageModel
     {
+        public Order Order { get; set; }
         public void OnGet()
         {
         }
