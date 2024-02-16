79429a08943ecec9491061eb54efa3de68d70161
Aml3t <fischerparsos@gmail.com>
2024-02-16 12:59:33 +0200

    Setting up the API

---
+++ Simple_eshop/Controllers/Api/SearchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_Eshop.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+    }
+}
